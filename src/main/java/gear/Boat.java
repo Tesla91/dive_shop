18e3eb8ed29bbf06fc7bcf6323b05c3aa1bad726
Daniel Kapitanov <dskapitanov@gmail.com>
2017-07-12 13:50:29 +0700

    added Boat and Fins classes lol

---
+++ src/main/java/gear/Boat.java
@@ -0,0 +1,14 @@
+package gear;
+
+/**
+ * Created by daniel on 12.07.17.
+ */
+public class Boat {
+    static private int numAvailable;
+    private int capacity;
+
+    Boat(int numAvailable, int capacity){
+        this.numAvailable=numAvailable;
+        this.capacity=capacity;
+    }
+}
