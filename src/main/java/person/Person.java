dfaf41d187f0e6d2e255d66cf4fb7f7ddb3c3887
Ryan Desmond <radesmond@gmail.com>
2017-07-12 13:47:19 +0700

    adding Person class

---
+++ src/main/java/person/Person.java
@@ -0,0 +1,41 @@
+package person;
+
+/**
+ * Created by ryandesmond on 7/12/17.
+ */
+public class Person {
+
+    private String first_name;
+    private String last_name;
+    private String age;
+
+    public Person(String first_name, String last_name, String age) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.age = age;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
